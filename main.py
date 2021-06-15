b5aff86c64283816489306200bfa0be89c8f80d7
Emanuele-M <81998297+Emanuele-M@users.noreply.github.com>
2021-06-16 00:54:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,72 +4,107 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import tensorforce as tf
+import tensorflow as tfl
 import matplotlib.pyplot as plt
-max_time = 100
-my_env = tf.Environment.create(environment='gym', level='MountainCarContinuous', max_episode_timesteps=max_time)
 
-#VPG Agent direttamente definito come da documentazione di TensorForce
 
-my_agent = tf.Agent.create(agent='reinforce', environment=my_env, batch_size=64, learning_rate=1e-3)
-
-#TensorForce Agent configurato per avere come obiettivo il Policy Gradient
-
-"""my_agent = tf.Agent.create(agent='tensorforce', environment=my_env, update=dict(unit='episodes', batch_size=64),
-                           optimizer=dict(optimizer='adam', learning_rate=1e-3), objective='policy_gradient',
-                           reward_estimation=dict(horizon=20, discount=0.99))"""
-
-# Training
-my_agent.reset()
-rewards = []
-for i in range(100):
-    for episode in range(100):
+def train(agent, environment, num_episodes):
+    tr_sum = 0.0
+    for episode in range(num_episodes):
         ep_states = list()
         ep_actions = list()
         ep_internals = list()
         ep_terminal = list()
         ep_rewards = list()
-        states = my_env.reset()
-        internals = my_agent.initial_internals()
+        states = environment.reset()
+        internals=agent.initial_internals()
         terminal = False
         while not terminal:
             ep_states.append(states)
             ep_internals.append(internals)
-            action, internals = my_agent.act(states=states, internals=internals, independent=True)
-            ep_actions.append(action)
-            states, terminal, reward = my_env.execute(actions=action)
+            actions, internals = agent.act(states=states, internals=internals, independent=True, deterministic=False)
+            ep_actions.append(actions)
+            states, terminal, reward = environment.execute(actions=actions)
             ep_terminal.append(terminal)
             ep_rewards.append(reward)
-        my_agent.experience(states=ep_states, internals=ep_internals, actions=ep_actions, reward=ep_rewards,
-                            terminal=ep_terminal)
-        my_agent.update()
+            tr_sum += reward
+        agent.experience(states=ep_states, actions=ep_actions, terminal=ep_terminal, reward=ep_rewards,
+                            internals=ep_internals,)
+        agent.update()
+        print(str(tr_sum))
+    return tr_sum
 
-    # Evaluation
-
-    sum_reward = 0.0
-    for t in range(100):
-        states = my_env.reset()
-        internals = my_agent.initial_internals()
+def evaluate(agent, environment, num_episodes):
+    ev_sum = 0.0
+    for t in range(num_episodes):
+        states = environment.reset()
+        internals = agent.initial_internals()
         terminal = False
         while not terminal:
-            actions, internals = my_agent.act(states=states, internals=internals, independent=True)
-            states, terminal, reward = my_env.execute(actions=actions)
-            sum_reward += reward
-    sum_reward = sum_reward/100.0
+            actions, internals = agent.act(states=states, internals=internals, independent=True)
+            states, terminal, reward = environment.execute(actions=actions)
+            ev_sum += reward
+        print(str(ev_sum))
+    return ev_sum
+
+max_time = 100
+my_env = tf.Environment.create(environment='gym', level='CartPole', max_episode_timesteps=max_time, visualize=True)
+actionSpace = my_env.actions()
+print(str(actionSpace))
+n_actions=actionSpace.get('num_values')
+print(str(n_actions))
+
+#VPG Agent direttamente definito come da documentazione di TensorForce
+
+#Policy network specification, two fully connected intermediate layers and an output layer
+network_spec = [dict(type='dense', size=64, activation='relu'),
+                dict(type='dense', size=64, activation='relu'),
+                dict(type='dense', size=n_actions, activation='softmax')]
+#clip = dict(type='clipping', lower= 1e-8, upper=1-1e-8)
+my_agent = tf.Agent.create(agent='reinforce', environment=my_env, batch_size=64, network=network_spec,
+                           use_beta_distribution=True, memory=10000, learning_rate=5e-4, discount=0.99, baseline='auto',
+                           baseline_optimizer=dict(optimizer='adam', learning_rate=5e-4))
+
+#TensorForce Agent configurato per avere come obiettivo il Policy Gradient
+
+
+#Train and evaluate the agent by using the act-experience-update TensorForce interface
+
+# Training
+my_agent.reset()
+train_rewards = []
+rewards = []
+
+for i in range(100):
+    training = train(my_agent, my_env, 100)
+    training = training/100.0
+    train_rewards.append(training)
+    print(str(training))
+    print('Training done at step ', i)
+    # Evaluation
+    sum_reward = evaluate(my_agent, my_env, 100)
+    sum_reward=sum_reward/100.0
     print(str(sum_reward))
     rewards.append(sum_reward)
-    print("Cleared training step ", i)
+    print('Cleared step ', i)
 
 my_env.close()
 my_agent.close()
 
 plt.plot(rewards)
 plt.xlabel('steps')
-plt.ylabel('avg_rewards')
+plt.ylabel('avg_rewards_eval')
 plt.show()
-
-
+plt.plot(train_rewards)
+plt.xlabel('steps')
+plt.ylabel('avg_rewards_train')
+plt.show()
 """runner = tf.execution.Runner(my_agent, my_env)
 
 runner.run(num_episodes=200)
 runner.run(num_episodes=100, evaluation=True)"""
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""my_agent = tf.Agent.create(agent='tensorforce', environment=my_env, update=dict(unit='episodes', batch_size=64),
+                           optimizer=dict(optimizer='adam', learning_rate=1e-3), objective='policy_gradient',
+                           reward_estimation=dict(horizon=20, discount=0.99))"""
